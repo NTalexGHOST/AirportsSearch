696f07abcfdbfb9cdff3fb0ec39c6e9927913a16
NTalexGHOST <alexpaa21@gmail.com>
2023-02-17 02:54:30 +0300

    Create basis of task without complying the requirements

---
+++ src/main/java/ru/darkalive/AirportsSearch.java
@@ -0,0 +1,42 @@
+package ru.darkalive;
+
+import java.io.*;
+
+public class AirportsSearch {
+
+    private String queryString;
+    private byte columnNum;
+    private long searchTime;
+
+    public AirportsSearch(String queryString, byte columnNum) {
+
+        this.queryString = queryString;
+        this.columnNum = columnNum;
+    }
+
+    public long getSearchTime() {
+        return searchTime;
+    }
+
+    public int startSearch() throws IOException {
+
+        int resultsCount = 0;
+        long startTime = System.nanoTime();
+
+        FileReader csvFileReader = new FileReader("airports.csv");
+        BufferedReader csvBufferedReader = new BufferedReader(csvFileReader);
+        String parsedLine;
+        while ((parsedLine = csvBufferedReader.readLine()) != null) {
+            String foundValue = parsedLine.split(",")[columnNum - 1];
+            if (foundValue.startsWith(queryString) || foundValue.startsWith("\"" + queryString)) {
+                System.out.println("\"" + foundValue + "\"[" + parsedLine + "]");
+                resultsCount++;
+            }
+        }
+
+        long endTime = System.nanoTime();
+        searchTime = (endTime - startTime) / 1000000;
+
+        return resultsCount;
+    }
+}
--- src/main/java/ru/darkalive/Main.java
+++ src/main/java/ru/darkalive/Main.java
@@ -1,7 +1,40 @@
 package ru.darkalive;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String queryString;
+        int resultsCount;
+
+        while (true) {
+
+            System.out.print("Введите строку: ");
+            InputStreamReader queryStreamReader = new InputStreamReader(System.in);
+            BufferedReader queryBufferedReader = new BufferedReader(queryStreamReader);
+
+            try {
+                queryString = queryBufferedReader.readLine();
+            } catch (IOException e) {
+                System.out.println("Ошибка!!!\n" + e.getMessage() + "\n");
+                continue;
+            }
+            if (queryString.equals("!quit")) break;
+
+            AirportsSearch airportsSearch = new AirportsSearch(queryString, Byte.parseByte(args[0]));
+            try {
+                resultsCount = airportsSearch.startSearch();
+            } catch (IOException e) {
+                System.out.println("Ошибка!!!\n" + e.getMessage() + "\n");
+                continue;
+            }
+
+            System.out.println("Количество найденных строк - " + resultsCount + "; " +
+                    "затраченное на поиск время - " + airportsSearch.getSearchTime() + " мс\n");
+        }
     }
 }
